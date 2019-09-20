91bf12a4ae7506376bef1881069eda155bc56fdf
Ruben Bergshoeff <rubicalme@gmail.com>
2019-09-20 14:07:59 +0200

    added dungeon window and movement

--- UnityProject/Assets/Scripts/DungeonController.cs
+++ UnityProject/Assets/Scripts/DungeonController.cs
@@ -7,10 +7,12 @@ using UnityEngine.UI;
 public class DungeonController : MonoBehaviour {
 
     [SerializeField] private Button characterButton = null;
+    [SerializeField] private Button characterButtonTop = null;
     [SerializeField] private CharacterController characterController = null;
     [SerializeField] private GameObject ConfirmScreen = null;
     [SerializeField] private TMPro.TextMeshProUGUI chancePercentageField = null;
     [SerializeField] private TMPro.TextMeshProUGUI nameField = null;
+    [SerializeField] private Image dungeonImage = null;
     private Dungeon currentDungeon;
 
     private void OnEnable() {
@@ -21,6 +23,7 @@ public class DungeonController : MonoBehaviour {
         ConfirmScreen.SetActive(true);
         nameField.text = dungeon.Name;
         chancePercentageField.text = GetChancePercentage(dungeon).ToString() + "%";
+        dungeonImage.sprite = dungeon.GetComponentInChildren<Image>().sprite;
         currentDungeon = dungeon;
     }
 
@@ -55,22 +58,56 @@ public class DungeonController : MonoBehaviour {
     }
 
     private IEnumerator ExploreDungeon() {
+        float exploreSpeed = 100;
         characterButton.interactable = false;
+        characterButtonTop.gameObject.SetActive(false);
         RectTransform characterPos = characterButton.GetComponent<RectTransform>();
         Vector2 startpos = characterPos.anchoredPosition;
         Transform parent = currentDungeon.transform.parent;
-        Debug.Log(parent);
-        RectTransform dungeonPos = parent.GetComponent<RectTransform>();
-        while (Vector2.Distance(characterPos.anchoredPosition, dungeonPos.anchoredPosition) > 0.1f) {
-            characterPos.anchoredPosition = Vector2.MoveTowards(characterPos.anchoredPosition, dungeonPos.anchoredPosition, 100 * Time.deltaTime);
+        Vector2 dungeonPos = parent.GetComponent<RectTransform>().anchoredPosition + new Vector2(0, -50);
+        while (Vector2.Distance(characterPos.anchoredPosition, dungeonPos) > 0.1f) {
+            characterPos.anchoredPosition = Vector2.MoveTowards(characterPos.anchoredPosition, dungeonPos, exploreSpeed * Time.deltaTime);
             yield return null;
         }
+
         yield return new WaitForSeconds(1);
+
+        float timeExploring = 0;
+        Vector2 randomPos = GetRandomPosNearCurrentDungeon();
+        while (timeExploring < 5) {
+            if (Vector2.Distance(characterPos.anchoredPosition, randomPos) > 0.1f) {
+                characterPos.anchoredPosition = Vector2.MoveTowards(characterPos.anchoredPosition, randomPos, exploreSpeed * Time.deltaTime);
+            } else {
+                randomPos = GetRandomPosNearCurrentDungeon();
+            }
+            timeExploring += Time.deltaTime;
+            yield return null;
+
+        }
+
+        // move outside
+        while (Vector2.Distance(characterPos.anchoredPosition, dungeonPos) > 0.1f) {
+            characterPos.anchoredPosition = Vector2.MoveTowards(characterPos.anchoredPosition, dungeonPos, exploreSpeed * Time.deltaTime);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(1);
+
+        // move home
         while (Vector2.Distance(characterPos.anchoredPosition, startpos) > 0.1f) {
-            characterPos.anchoredPosition = Vector2.MoveTowards(characterPos.anchoredPosition, startpos, 100 * Time.deltaTime);
+            characterPos.anchoredPosition = Vector2.MoveTowards(characterPos.anchoredPosition, startpos, exploreSpeed * Time.deltaTime);
             yield return null;
         }
         characterButton.interactable = true;
+        characterButtonTop.gameObject.SetActive(true);
+    }
+
+    private Vector2 GetRandomPosNearCurrentDungeon() {
+        Transform parent = currentDungeon.transform.parent;
+        RectTransform dungeonPos = parent.GetComponent<RectTransform>();
+        Vector2 pos = dungeonPos.anchoredPosition;
+        pos += new Vector2(UnityEngine.Random.Range(0, 60f), UnityEngine.Random.Range(0, 60f));
+        return pos;
     }
 
 }
