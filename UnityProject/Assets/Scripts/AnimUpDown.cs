482f7d16c522efff5bcbcb69f060b849bc4dda74
Ruben Bergshoeff <rubicalme@gmail.com>
2019-09-20 13:17:59 +0200

    added map buttons, added small updown anim to character button

---
+++ UnityProject/Assets/Scripts/AnimUpDown.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimUpDown : MonoBehaviour {
+    [SerializeField] private float frequency = 1;
+    [SerializeField] private float distMultiplier = 10;
+    private RectTransform rectTransform;
+
+    private void Awake() {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    void Update() {
+        Vector2 tempPos = rectTransform.anchoredPosition;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * distMultiplier;
+        rectTransform.anchoredPosition = tempPos;
+    }
+}
